5a295b4d6e7f3f535c84cbc9e05168a9ed516286
MichealC03 <micheal.callahan@icloud.com>
2024-12-10 10:10:38 -0700

    changing around all_images to process at the start rather than try to process all later

---
+++ Image Blocker CNN - Project 7/Plugin/content.js
@@ -0,0 +1,4 @@
+const imgs = document.getElementsByTagName('img');
+for ( let i = 0; i < imgs.length; i++) {
+    imgs[i].src = 'https://media.licdn.com/dms/image/v2/D5603AQHPvFDtUSo3-A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1669181171687?e=1738800000&v=beta&t=PU83cSQecZJPExgAwj7fq8aKhhcvS0L2MCowGHbsQYw';
+}
