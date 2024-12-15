5bc98825d5961b452129f6f9ae5ff42453387282
MichealC03 <micheal.callahan@icloud.com>
2024-12-15 10:41:25 -0700

    ignore the virtual env

--- Image Blocker CNN - Project 7/Plugin/content.js
+++ Image Blocker CNN - Project 7/Plugin/content.js
@@ -1,4 +1,12 @@
-const imgs = document.getElementsByTagName('img');
-for ( let i = 0; i < imgs.length; i++) {
-    imgs[i].src = 'https://media.licdn.com/dms/image/v2/D5603AQHPvFDtUSo3-A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1669181171687?e=1738800000&v=beta&t=PU83cSQecZJPExgAwj7fq8aKhhcvS0L2MCowGHbsQYw';
+// const imgs = document.getElementsByTagName('img');
+// for ( let i = 0; i < imgs.length; i++) {
+//     imgs[i].src = 'https://media.licdn.com/dms/image/v2/D5603AQHPvFDtUSo3-A/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1669181171687?e=1738800000&v=beta&t=PU83cSQecZJPExgAwj7fq8aKhhcvS0L2MCowGHbsQYw';
+// }
+
+let model;
+
+// Load the model
+async function loadModel() {
+  model = await tf.loadLayersModel(chrome.runtime.getURL('tfjs_model/model.json'));
+  console.log("Model loaded!");
 }
